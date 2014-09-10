3016af9b00bbb053f2c7e69e56e84a160188360f
Meg Sandman <margaret.sandman@gmail.com>
2014-09-10 08:22:45 -0700

    add week 5 technical code

--- blog-posts/week5_technical_code.rb
+++ blog-posts/week5_technical_code.rb
@@ -1,3 +1,4 @@
+# ORIGINAL CODE
 
 class Dog
 
@@ -19,28 +20,48 @@ class Dog
   def update_fave_treat(new_fave_treat)
     @fave_treat = new_fave_treat
   end
+
 end
+
+# REFACTORED CODE
+
+class Dog
+
+  #defines reader and writer instance methods for @name and @fave_treat
+  attr_accessor :name, :fave_treat
+  # defines a reader instance method for @age
+  attr_reader :age
+
+  def initialize(name, age, fave_treat)
+    @name = name
+    @age = age
+    @fave_treat = fave_treat
+  end
+
+  def one_year_older
+    @age += 1
+    puts "Happy Birthday, #{@name}! #{@name} is now #{@age} years old!"
+  end
+
+  def Dog.oldest(*dogs)
+    dogs.max_by(&:age)
+  end
+
+end
+
+# TEST CODE
 
 brie = Dog.new("Brie", 13, "lettuce ribs")
 
-brie.one_year_older
+p brie.one_year_older
 # => Happy Birthday, Brie! Brie is now 14 years old!
-brie.one_year_older
+p brie.one_year_older
 # => Happy Birthday, Brie! Brie is now 15 years old!
 
-
- def get_fave_treat
-    puts "#{@name}'s favorite treat is #{@fave_treat}."
-  end
-
-  def update_fave_treat(new_fave_treat)
-    @fave_treat = new_fave_treat
-  end
-
-brie.get_fave_treat
-# => "Brie's favorite treat is lettuce ribs."
-brie.update_fave_treat("string cheese")
-brie.get_fave_treat
-# => "Brie's favorite treat is string cheese."
+p brie.fave_treat
+# => "lettuce ribs"
+brie.fave_treat = "string cheese"
+p brie.fave_treat
+# => "string cheese"
 
 
