d6022ac2d31747f9b459be6ce4f0489014714c6c
Meg Sandman <margaret.sandman@gmail.com>
2014-09-10 08:01:43 -0700

    Final edits to post

---
+++ blog-posts/week6_technical_code.rb
@@ -0,0 +1,47 @@
+class Dog
+
+  #defines reader and writer instance methods for @name and @fave_treat
+  attr_accessor :name, :fave_treat
+  # defines a reader instance method for @age
+  attr_reader :age
+
+  def initialize(name, age, fave_treat)
+    @name = name
+    @age = age
+    @fave_treat = fave_treat
+  end
+
+  # instance method
+  def one_year_older
+    @age += 1
+    puts "Happy Birthday, #{@name}! #{@name} is now #{@age} years old!"
+  end
+
+  # class method
+  def Dog.oldest(*dogs)
+    dogs.max_by(&:age)
+  end
+
+end
+
+
+# TEST CODE
+
+brie = Dog.new("Brie", 13, "lettuce ribs")
+luna = Dog.new("Luna", 4, "peanut butter")
+
+p brie.one_year_older
+# => Happy Birthday, Brie! Brie is now 14 years old!
+p brie.one_year_older
+# => Happy Birthday, Brie! Brie is now 15 years old!
+
+p brie.fave_treat
+# => "lettuce ribs"
+brie.fave_treat = "string cheese"
+p brie.fave_treat
+# => "string cheese"
+
+Dog.oldest(brie, luna)
+# => brie
+Dog.oldest
+# => nil
